6f9e4f6493a39920c39bd438e390f60388aa3fb5
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-09 21:36:59 +0200

    chore: Added JSDoc examples

--- src/Text.ts
+++ src/Text.ts
@@ -26,6 +26,9 @@ export class Text extends String {
 
   /**
    * Creates a new instance of the builder.
+   *
+   * @example
+   * Text.make('hello world'); // hello world
    */
   static make(...fragments: Input[]): Text {
     return new Text(...fragments);
@@ -33,6 +36,9 @@ export class Text extends String {
 
   /**
    * Generates a random alpha-numeric string.
+   *
+   * @example
+   * Text.random(8); // 0ONCLOnZ
    */
   static random(length: number = 16): Text {
     const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -61,6 +67,14 @@ export class Text extends String {
    * Appends a space character.
    *
    * @param count Amount of spaces to add.
+   *
+   * @example
+   * Text.make()
+   *		.append('[')
+   *		.time('=', 5)
+   *		.space(5)
+   *		.append('] 50%');
+   * // [=====     ] 50%
    */
   space(count: number = 1): this {
     return this.times(' ', count);
@@ -70,6 +84,12 @@ export class Text extends String {
    * Appends a new line character.
    *
    * @param count Amount of spaces to add.
+   *
+   * @example
+   * Text.make('Hello')
+   *		.newLine()
+   *		.append('world');
+   * // Hello\nworld
    */
   newLine(count: number = 1): this {
     return this.times('\n', count);
@@ -79,6 +99,12 @@ export class Text extends String {
    * Appends a new line character.
    *
    * @param count Amount of spaces to add.
+   *
+   * @example
+   * Text.make('Hello')
+   *		.nl()
+   *		.append('world');
+   * // Hello\nworld
    */
   nl(count: number = 1): this {
     return this.times('\n', count);
@@ -89,6 +115,12 @@ export class Text extends String {
    *
    * @param input The given input.
    * @param count Times to append.
+   *
+   * @example
+   * Text.make('He')
+   *		.times('l', 8)
+   *		.append('o');
+   * // Hellllllllo
    */
   times(input: Input, count: number = 1): this {
     for (let i = 0; i < Math.max(count, 1); i++) {
@@ -100,6 +132,11 @@ export class Text extends String {
 
   /**
    * Appends the given input to the builder.
+   *
+   * @example
+   * Text.make('Hello')
+   *		.append('world')
+   * // Helloworld
    */
   append(...input: Input[]): this {
     this._fragments.push(...this.fragmentify(input));
@@ -108,7 +145,12 @@ export class Text extends String {
   }
 
   /**
-   * Returns a Text that contains the concatenation of the given string.
+   * Concatenates the given inputs to the current text.
+   *
+   * @example
+   * Text.make()
+   *		.concat('hello', 'world');
+   * // helloworld
    */
   // @ts-ignore This if fine.
   concat(...input: Input[]): this {
@@ -116,7 +158,12 @@ export class Text extends String {
   }
 
   /**
-   * Appends the given input to the builder.
+   * Appends a new line and the given input to the builder.
+   *
+   * @example
+   * This.make('hello')
+   *		.appendLine('world');
+   * // hello\nworld
    */
   appendLine(...input: Input[]): this {
     return this.append('\n', ...this.fragmentify(input));
@@ -124,6 +171,11 @@ export class Text extends String {
 
   /**
    * Appends the given input to the builder.
+   *
+   * @example
+   * This.make('hello')
+   *		.line('world');
+   * // hello\nworld
    */
   line(...input: Input[]): this {
     return this.appendLine(...input);
@@ -131,6 +183,11 @@ export class Text extends String {
 
   /**
    * Prepends the given input to the builder.
+   *
+   * @example
+   * This.make('world')
+   *		.prepend('hello');
+   * // helloworld
    */
   prepend(...input: Input[]): this {
     this._fragments = this.fragmentify(input).concat(this._fragments);
@@ -140,20 +197,35 @@ export class Text extends String {
 
   /**
    * Prepends the given input to the builder.
+   *
+   * @example
+   * This.make('world')
+   *		.prependLine('hello');
+   * // hello\nworld
    */
   prependLine(...input: Input[]): this {
     return this.prepend(...this.fragmentify(input), '\n');
   }
 
   /**
-   * Appends the given lines to the
+   * Appends the given lines to the builder.
+   *
+   * @example
+   * This.make()
+   *		.appendLines('hello', 'world');
+   * // \nhello\nworld
    */
   appendLines(...input: Input[]): this {
     return this.append(...input.map(line => `\n${line}`));
   }
 
   /**
-   * Prepends the given lines to the
+   * Prepends the given lines to the builder.
+   *
+   * @example
+   * This.make()
+   *		.prependLines('hello', 'world');
+   * // hello\nworld
    */
   prependLines(...input: Input[]): this {
     const lines = input.map(line => `\n${line}`);
@@ -181,6 +253,11 @@ export class Text extends String {
 
   /**
    * Gets the portion of a string before the first occurrence of the given value.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.before(' world');
+   * // Hello
    */
   before(search: string): Text {
     if (0 === search.length) {
@@ -192,6 +269,11 @@ export class Text extends String {
 
   /**
    * Gets the portion of a string before the last occurrence of the given value.
+   *
+   * @example
+   * Text.make('hello/world/foo.txt')
+   * 	.beforeLast('/')
+   * // hello/world
    */
   beforeLast(search: string): Text {
     if (0 === search.length) {
@@ -209,6 +291,11 @@ export class Text extends String {
 
   /**
    * Return the remainder of a string after the first occurrence of a given value.
+   *
+   * @example
+   * Text.make('hello world')
+   * 	.after('hello ')
+   * // world
    */
   after(search: string): Text {
     if (0 === search.length) {
@@ -226,6 +313,11 @@ export class Text extends String {
 
   /**
    * Return the remainder of a string after the last occurrence of a given value.
+   *
+   * @example
+   * Text.make('hello/world/how/are/you.txt')
+   * 	.afterLast('/')
+   * // you.txt
    */
   afterLast(search: string): Text {
     if (0 === search.length) {
@@ -243,13 +335,24 @@ export class Text extends String {
 
   /**
    * Get the portion of a string between two given values.
+   *
+   * @example
+   * Text.make('blowout')
+   * 	.between('l', 'u')
+   * // owo
    */
   between(from: string, to: string): Text {
     return this.after(from).beforeLast(to);
   }
 
   /**
-   * Get the portion of a string between two given values.
+   * Get the portion of a string between the last occurrence of the first value
+   * and the first occurence of the second one.
+   *
+   * @example
+   * Text.make('aabbcc')
+   * 	.inside('a', 'c')
+   * // bb
    */
   inside(from: string, to: string): Text {
     return this.afterLast(from).before(to);
@@ -257,6 +360,15 @@ export class Text extends String {
 
   /**
    * Cap a string with a single instance of a given value.
+   *
+   * @example
+   * Text.make('hello/world')
+   * 	.finish('/')
+   * // hello/world/
+   *
+   * Text.make('hello/world/')
+   * 	.finish('/')
+   * // hello/world/
    */
   finish(value: string): Text {
     return this.endsWith(value) ? this : this.append(value);
@@ -264,6 +376,11 @@ export class Text extends String {
 
   /**
    * Determines if a given string is a valid UUID.
+   *
+   * @example
+   * Text.make('8fc5b4a2-aa85-11ea-bb37-0242ac130002')
+   * 	.isUuid()
+   * // true
    */
   isUuid(): Boolean {
     return (
@@ -273,6 +390,11 @@ export class Text extends String {
 
   /**
    * Converts all the alphabetic characters in a string to lowercase.
+   *
+   * @example
+   * Text.make('HELLO WORLD')
+   * 	.lower()
+   * // hello world
    */
   lower(): Text {
     return Text.make(this.toLowerCase());
@@ -280,6 +402,11 @@ export class Text extends String {
 
   /**
    * Converts all the alphabetic characters in a string to uppercase.
+   *
+   * @example
+   * Text.make('hello world')
+   * 	.lower()
+   * // HELLO WORLD
    */
   upper(): Text {
     return Text.make(this.toUpperCase());
@@ -287,6 +414,11 @@ export class Text extends String {
 
   /**
    * Change the first character to upper case.
+   *
+   * @example
+   * Text.make('hello world')
+   * 	.lower()
+   * // Hello world
    */
   upperFirst(): Text {
     return Text.make(this.toString().charAt(0).toUpperCase() + this.toString().slice(1));
@@ -294,6 +426,11 @@ export class Text extends String {
 
   /**
    * Change the first character to lower case.
+   *
+   * @example
+   * Text.make('HELLO WORLD')
+   * 	.lower()
+   * // hELLO WORLD
    */
   lowerFirst(): Text {
     return Text.make(this.toString().charAt(0).toLowerCase() + this.toString().slice(1));
@@ -302,11 +439,14 @@ export class Text extends String {
   /**
    * Splits `string` into an array of its words.
    *
-   * words('fred, barney, & pebbles')
-   * // => ['fred', 'barney', 'pebbles']
+   * @example
+   * Text.make('Kaguya, Komi & Holo')
+   * 	.words()
+   * // ['Kaguya', 'Komi', 'Holo']
    *
-   * words('fred, barney, & pebbles', /[^, ]+/g)
-   * // => ['fred', 'barney', '&', 'pebbles']
+   * Text.make('Kaguya, Komi, & Holo')
+   * 	.words(/[^, ]+/g)
+   * // ['Kaguya', 'Komi', '&', 'Holo']
    */
   words(pattern?: RegExp | string): string[] {
     return words(this.toString(), pattern);
@@ -314,6 +454,11 @@ export class Text extends String {
 
   /**
    * Converts the text to `kebab-case`.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.kebabCase()
+   * // hello-world
    */
   kebabCase(): Text {
     return this.case(
@@ -323,6 +468,11 @@ export class Text extends String {
 
   /**
    * Converts the text to `snake_case`.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.snakeCase()
+   * // hello_world
    */
   snakeCase(): Text {
     return this.case(
@@ -332,6 +482,11 @@ export class Text extends String {
 
   /**
    * Converts the text to `camelCase`.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.camelCase()
+   * // helloWorld
    */
   camelCase(): Text {
     return this.case((result, word, index) => {
@@ -342,6 +497,11 @@ export class Text extends String {
 
   /**
    * Converts the text to `pascalCase`.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.pascalCase()
+   * // HelloWorld
    */
   pascalCase(): Text {
     return this.case((result, word) => {
@@ -355,6 +515,17 @@ export class Text extends String {
    * Converts the text to a case determined by the given reducer.
    * The reducer parameters are the current string, the current word and the
    * current word's index.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.case((result, word) => {
+   * 		word = [...word]
+   * 			.map((char, i) => (i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()))
+   * 			.join('');
+   *
+   * 		return result + (result ? ' ' : '') + word;
+   * 	})
+   * // HeLlO WoRlD
    */
   case(reducer: (result: string, word: string, index: number) => string): Text {
     return Text.make(words(this.replace(/['\u2019]/g, '')).reduce(reducer, ''));
@@ -368,6 +539,11 @@ export class Text extends String {
 
   /**
    * Returns each fragment of this builder as a string, separated by the given string.
+   *
+   * @example
+   * Text.make('Hello', 'world')
+   * 	.join(', ')
+   * // Hello, world
    */
   join(separator: string = ''): string {
     return this._fragments.join(separator);
@@ -375,6 +551,11 @@ export class Text extends String {
 
   /**
    * Shortcut for `.toString()`.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.str()
+   * // Hello world
    */
   str(): string {
     return this.toString();
@@ -382,11 +563,13 @@ export class Text extends String {
 
   /**
    * Returns the builder as a string.
+   *
+   * @example
+   * Text.make('Hello world')
+   * 	.toString()
+   * // Hello world
    */
   toString(): string {
     return this.join();
   }
 }
-
-const t = new Text('ze"z');
-Text.make('').concat(t);
